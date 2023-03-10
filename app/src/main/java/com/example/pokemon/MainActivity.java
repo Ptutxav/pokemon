4f0f0cdf110fb1fb5fa9fe9d941e1669229421dd
Ptutxav <xav.cimb@gmail.com>
2023-03-10 13:32:33 +0100

    FIRST COMMIT

---
+++ app/src/main/java/com/example/pokemon/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.pokemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Bonjour !!!", Toast.LENGTH_SHORT).show();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Clic", Toast.LENGTH_SHORT).show();
+                Log.v("Clic", "Clic");
+            }
+        });
+
+    }
+}
